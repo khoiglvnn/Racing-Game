130b5a03bab31ee8bc1dad2641ec2f26d4c39ca0
Racing Game <khoiglvnn@gmail.com>
2020-09-22 20:48:59 +0700

    first commit

---
+++ Assets/Scripts/HalfPointTrigger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HalfPointTrigger : MonoBehaviour
+{
+    public GameObject LapCompeteTrig;
+    public GameObject HalfLapTrig;
+
+    void OnTriggerEnter ()
+    {
+        LapCompeteTrig.SetActive(true);
+        HalfLapTrig.SetActive(false);
+    }
+}
